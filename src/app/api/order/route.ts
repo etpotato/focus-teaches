76f346db5f87437d04393501a324700273e6c7c6
Pavel Mareev <mareev.pv@gmail.com>
2024-02-07 08:53:47 +0100

    feat: add logs

--- src/app/api/order/route.ts
+++ src/app/api/order/route.ts
@@ -7,17 +7,19 @@ export async function POST(req: Request) {
     const formData = await req.formData()
     const formDataObject = Object.fromEntries(formData.entries())
     const order = await orderSchema.parseAsync(formDataObject)
-    console.log(order);
 
-    getTgService().send(`
+    await getTgService().send(`
 
-<b>new order:</b>
-name: ${order.name}
-phone: ${order.phone}
+    <b>new order:</b>
+    name: ${order.name}
+    phone: ${order.phone}
     `)
+
+    console.log('Order success', order);
 
     return NextResponse.json({ success: true }, { status: 200 })
   } catch (error) {
+    console.error('Order error', error)
     return NextResponse.json({ success: false }, { status: 400 })
   }
 }
