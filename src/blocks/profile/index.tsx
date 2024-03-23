c3fd0c3d636e7fed2086004b9569e07e99c4e7b8
Микляева Ева <miklyayeva09@mail.ru>
2024-03-23 22:58:54 +0300

    Добавляет картинку без фона

--- src/blocks/profile/index.tsx
+++ src/blocks/profile/index.tsx
@@ -25,6 +25,7 @@ export const Profile = () => {
 
     return (
         <section className={styles.profile}>
+            <div>
                 <blockquote className={styles.blockquote}>
                     <p className={styles.quote}>&Prime;{data.profile.quote}&Prime;</p>
                     <cite className={styles.cite}>{RenderLineBreaks(data.profile.author)}</cite>
@@ -32,8 +33,9 @@ export const Profile = () => {
                 <Text className={styles.description} tag='span'>
                     {RenderLineBreaks(data.profile.achievements)}
                 </Text>
+            </div>
+            <img className={styles.image} src="/img/profile/profile.png"  width="770" height="831" />
             <Button tag='a' href={data.profile.link} className={styles.button}>{data.profile.cta}</Button>
-            <Image className={styles.image} src={`/img/profile/${data.profile.img.id}.png`} alt={data.profile.img.alt} width="770" height="831" />
         </section>
     );
 };
